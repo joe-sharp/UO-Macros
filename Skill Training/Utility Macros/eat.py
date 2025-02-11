17cff705d918dd84f5ec5b9811115efa041e105a
Joe Sharp <joesharp13@gmail.com>
2025-02-11 16:28:01 -0600

    Add comment

--- Skill Training/Utility Macros/eat.py
+++ Skill Training/Utility Macros/eat.py
@@ -4,6 +4,8 @@
 # Shard: UOA
 # Date: Tue Feb 11 2025
 
+# Note: This must be set to "Run in background" if called from other macros such as magery_and_meditation
+
 UseType(0x136f)
 UseType(0x2827)
 Pause(1000)
