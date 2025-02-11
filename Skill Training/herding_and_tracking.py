a4219d0b3c561c080be21ec4694aea253b6d19f8
Joe Sharp <joesharp13@gmail.com>
2025-02-11 16:12:35 -0600

    Add training macros

---
+++ Skill Training/herding_and_tracking.py
@@ -0,0 +1,27 @@
+# Name: Herding and Tracking
+# Description: Train Harding and Tracking at the same time. Periodically you will need to herd more difficult creatures.
+# Author: Telfer
+# Shard: UOA
+# Date: Tue Feb 11 2025
+
+def herd():
+    UseObject(0x40098035)
+    WaitForTarget(5000)
+    Target("last")
+    WaitForTarget(5000)
+    TargetTileOffset(0, 0, 0)
+
+def track():
+    UseSkill("Trackin")
+    WaitForGump(0xb16e7d71, 5000)
+    ReplyGump(0xb16e7d71, 5)
+
+def eat_and_drink():
+    UseObject(0x4001da02)
+    UseObject(0x4001cdf5)
+
+for i in range(20):
+    herd()
+    track()
+
+eat_and_drink()
---
+++ Skill Training/magery_and_meditation.py
@@ -0,0 +1,36 @@
+# Name: Training Magery
+# Description: Train magery skill until the goal you want. Modified from a macro by Lissandro
+# Author: Telfer
+# Shard: UOA
+# Date: Tue Feb 11 2025
+
+
+GOAL = 100
+
+while Skill("Mager") < GOAL:
+    if Mana("self") < 50:
+        # Replace this with a Macro that eats and drinks whatever you want and ends with a `Pause(1000)`.
+        # Or simply remove it if you don't need to eat/drink.
+        PlayMacro("telfer-eat")
+        while Mana() < MaxMana("self"):
+            UseSkill("Meditatio")
+            Pause(10000)
+
+    if Skill("Mager") < 45:
+        Cast("Bless", "self")
+        Pause(1800)
+    if Skill("Mager") >= 45 and Skill("Mager") < 55:
+        Cast("greater heal", "self")
+        Pause(2100)
+    if Skill("Mager") >= 55 and Skill("Mager") < 65:
+        Cast("Poison Field", "self")
+        Pause(2400)
+    if Skill("Mager") >= 65 and Skill("Mager") < 75:
+        Cast("Invisibility", "Self")
+        Pause(2700)
+    if Skill("Mager") >= 75 and Skill("Mager") < 90:
+        Cast("Mass Dispel", "Self")
+        Pause(3000)
+    if Skill("Mager") >= 90 and Skill("Mager") < 120:
+        Cast("earthquake")
+        Pause(3300)
